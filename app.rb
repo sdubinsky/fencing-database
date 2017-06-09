412dc76961f02fa6b68de37296ac9cd62cfee891
Scott Dubinsky <smdubinsky@gmail.com>
2017-06-09 18:22:59 -0400

    config changes

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
+require 'models/init.rb'
 
 configure :development do
   set :show_exceptions, true
+  DB = Sequel.connect 'postgres://localhost:5432/fencingstats'
 end
 
+set :root, File.dirname(__FILE__)
 
 get '/' do
   @score_strip_locations = [:fotl_warning_strip, :fotl_half, :middle, :fotr_half, :fotr_warning_strip]
   @score_body_locations = [:hand, :front_arm, :torso, :head, :front_leg, :foot, :back_arm, :back_leg]
   erb :clip_form
+end
+
+get '/submit/?' do 
+  redirect '/'
 end
