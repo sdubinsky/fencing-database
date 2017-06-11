1c704a6bf0292ab91e8c2fec75b162a1f6842962
Scott Dubinsky <smdubinsky@gmail.com>
2017-06-11 07:52:05 -0400

    add submit method

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ set :root, File.dirname(__FILE__)
 get '/' do
   @score_strip_locations = [:fotl_warning_strip, :fotl_half, :middle, :fotr_half, :fotr_warning_strip]
   @score_body_locations = [:hand, :front_arm, :torso, :head, :front_leg, :foot, :back_arm, :back_leg]
-  @gfycat_id = Gfycat.random_gfycat_id
+  @gfycat = Gfycat.random_gfycat_id
   erb :clip_form
 end
 
@@ -21,9 +21,18 @@ get '/submit/?' do
     fotr_name: params['fotr-name'],
     initiated: params['initiated-action'],
     strip_location: params['strip-location'],
-    body_location: params['score-body-select']
+    body_location: params['score-body-select'],
+    gfycat_id: params['gfycat-id']
   )
   response.save
   redirect '/'
 end
 
+get '/submit_gfycat/?' do
+  gfycat = Gfycat.new(
+    gfycat_gfy_id: params['gfycat_id'],
+    tournament: params['tournament'],
+    weapon: params['weapon'],
+    gender: params['gender']
+  )
+end
