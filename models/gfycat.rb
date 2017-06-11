71b62b0c48033958cc7ad31bb6604f32c9a67f28
Scott Dubinsky <smdubinsky@gmail.com>
2017-06-11 07:52:15 -0400

    raise error if gfy not found

--- models/gfycat.rb
+++ models/gfycat.rb
@@ -2,6 +2,8 @@ class Gfycat < Sequel::Model
   def self.random_gfycat_id
     maxval = Gfycat.count
     random_id = rand(maxval) + 1
-    self.where(id: random_id).first.gfycat_gfy_id
+    gfycat = self.where(id: random_id).first
+    raise RuntimeError.new("no gfycats found") if not gfycat
+    gfycat
   end
 end
