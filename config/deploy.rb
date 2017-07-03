f812583899c63a885add15d5d52952bb2ec1abf9
Scott Dubinsky <smdubinsky@gmail.com>
2017-07-04 00:15:15 +0300

    include log dir in capistrano deploy shared dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,4 +7,5 @@ set :repo_url, 'git@github.com:sdubinsky/fencing-database.git'
 set :deploy_to, '/home/ec2-user/fencing-database'
 
 set :linked_files, fetch(:linked_files, []).push('config.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log')
 set :bundle_bins, fetch(:bundle_bins, []).push('ruby')
