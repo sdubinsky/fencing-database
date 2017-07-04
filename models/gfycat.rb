795cee05a1a6502764f3542832e79d8a49a16e03
Scott Dubinsky <smdubinsky@gmail.com>
2017-07-05 01:39:37 +0300

    correct way to take a random gfycat

--- models/gfycat.rb
+++ models/gfycat.rb
@@ -1,9 +1,7 @@
 class Gfycat < Sequel::Model
   one_to_many :form_responses
   def self.random_gfycat_id
-    maxval = Gfycat.count
-    random_id = rand(maxval) + 1
-    gfycat = self.where(id: random_id).first
+    gfycat = Gfycat.all.sample
     raise RuntimeError.new("no gfycats found") if not gfycat
     gfycat
   end
