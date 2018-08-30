418e8dc4aff085ca2967a3f40bf99e7cfcac6a2a
Scott Dubinsky <smdubinsky@gmail.com>
2018-08-30 20:53:59 +0300

    add sql header line

--- get_fencer_names.rb
+++ get_fencer_names.rb
@@ -1,7 +1,7 @@
 f1 = File.open("fencers_names/all_fencers.html", 'rb')
-f2 = File.open("fencers_names/fencers_sanitized.html", 'w')
-
+f2 = File.open("fencers_names/fencers.sql", 'w')
+f2.puts "insert into fencers (last_name, first_name, nationality, birthday, gender) values"
 f1.each.select{|a| a.include? "<td>"}.each do |line|
-  l2 = line.gsub(/<.*?>/, ",").sub(/(\s.[a-z])/, ',\1').gsub(",,", ",").gsub(",,", ",").gsub(",\n", "\n").gsub(/\A,/, "").gsub(", ", ",").gsub(",", "','").strip
+  l2 = line.gsub(/<.*?>/, ",").sub(/(\s.[^A-Z])/, ',\1').gsub(",,", ",").gsub(",,", ",").gsub(",\n", "\n").gsub(/\A,/, "").gsub(", ", ",").gsub(",", "','").gsub("&nbsp;", "").strip
   f2.puts "('#{l2}'),"
 end
