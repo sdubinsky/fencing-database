8fe9a86609f8c8b89fece21d4e8eaa13e532531f
Scott Dubinsky <smdubinsky@gmail.com>
2018-09-02 14:00:16 +0300

    update to original normalize_names script

--- normalize_names.rb
+++ normalize_names.rb
@@ -19,30 +19,28 @@ connstr = "postgres://#{login}#{db_config['db_address']}/#{db_config['db_name']}
 DB = Sequel.connect(connstr)
 
 require './models/init'
-
-def check_name gfy_name
+#Finds matches where:
+#1. The only name available is the last name, and it has exactly one match
+#2. There's a last name and some of the first name, and it has exactly one match
+def check_names gfy_name
   names1 = Fencer.where(last_name: gfy_name)
   return names1.first if names1 and names1.count == 1
-  names2 = Fencer.where(last_name: gfy_name.split(" ")[0..-1].join(" "))
+  names2 = Fencer.where(last_name: gfy_name.split(" ")[0...-1], first_name: /^#{gfy_name.split(" ")[-1]}/i)
   return names2.first if names2 and names2.count == 1
-  names3 = Fencer.where(last_name: gfy_name, first_name: /$#{gfy_name.split(" ")[-1][0]}/)
-  return names3.first if names3 and names3.count == 1
 end
 #first pass - just check if last name is a perfect match, and that only one fencer has that last name
-Gfycat.each do |gfy|
-  right_name = check_name gfy.fotr_name
+Gfycat.where(valid: true).each do |gfy|
+  right_name = check_names gfy.fotr_name
   if right_name
-    puts "#{gfy.fotr_name} matches with #{right_name.name}"
-    # gfy.update(
-    #   right_fencer_id: right_name.id
-    # )
+     gfy.update(
+      right_fencer_id: right_name.id
+    )
   end
 
-  left_name = check_name gfy.fotl_name
+  left_name = check_names gfy.fotl_name
   if left_name
-    puts "#{gfy.fotl_name} matches with #{left_name.name}"
-    # gfy.update(
-    #   left_fencer_id: left_name.first.id
-    # )
+     gfy.update(
+       left_fencer_id: left_name.id
+    )
   end
 end
