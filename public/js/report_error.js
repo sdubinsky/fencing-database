5ad609a83f5222e19923377c2f34b095ce3bd538
Shalom Dubinsky <smdubinsky@gmail.com>
2020-06-17 20:24:15 +0300

    report errors, say thank you

---
+++ public/js/report_error.js
@@ -0,0 +1,12 @@
+function reportError(gfy_id){
+    var body = JSON.stringify({
+        'gfy_id': gfy_id
+    })
+    var request = new XMLHttpRequest();
+    request.open('POST', '/error_report');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(body);
+    var error_elem = document.getElementById('report-error');
+    error_elem.innerHTML = 'Thank you!';
+    return false;
+}
