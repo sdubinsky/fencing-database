26b7f2c1784fb2559cfa43c7070a9c0b6acce78b
Shalom Dubinsky <smdubinsky@gmail.com>
2020-08-26 00:55:28 +0300

    add kazan 2020

--- download_tournament_entries.rb
+++ download_tournament_entries.rb
@@ -77,7 +77,8 @@ url_ids = {'budapest2020': ['https://fie.org/competition/2020/112/entry/pdf?lang
                              'https://fie.org/competition/2020/458/entry/pdf?lang=en'],
            'moscowsabre2012': ['https://fie.org/competition/2012/156/entry/pdf?lang=en',
                                'https://fie.org/competition/2012/468/entry/pdf?lang=en'],
-           'katowicefoil2019': ['https://fie.org/competition/2019/118/entry/pdf?lang=en']
+           'katowicefoil2019': ['https://fie.org/competition/2019/118/entry/pdf?lang=en'],
+           'kazanfoil2020': ['https://fie.org/competition/2020/129/entry/pdf?lang=en']
 }
 
 Sequel.connect connstr do |db|
@@ -109,9 +110,10 @@ url_ids.each do |tournament_key, urls|
               fie_id: entry[4]
             )
           end
+          select = Fencer.select(:id, tournament.id).where(fie_id: licenses)
+          puts db[:fencers_tournaments].insert([:fencers_id, :tournaments_id], select)
         end
-        select = Fencer.select(:id, tournament.id).where(fie_id: licenses)
-        puts db[:fencers_tournaments].insert([:fencers_id, :tournaments_id], select)
+
       end
     end
   end
